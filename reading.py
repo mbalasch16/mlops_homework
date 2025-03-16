f53ba63699bf80e51294caa30af6bda042311907
EC2 Default User <ec2-user@ip-172-31-77-48.ec2.internal>
2025-03-16 18:31:13 +0000

    updated directory location of file

--- reading.py
+++ reading.py
@@ -1,7 +1,7 @@
 
 print("reading from a file")
 
-with open("/mlops_sentiment_lab/email_text.txt",'r') as file:
+with open("/homework2/email_text.txt",'r') as file:
     lines = file.readlines()
     
 print(lines)
